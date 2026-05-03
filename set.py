fe3431a88b790863ddce58d0543a8693ea784ef1
shendesaanvi-ctrl <shendesaanvi@gmail.com>
2026-05-03 19:04:34 +0530

    set.py

---
+++ set.py
@@ -0,0 +1,15 @@
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print("Set A:", A)
+print("Set B:", B)
+
+# Union
+print("Union:", A | B)
+
+# Intersection
+print("Intersection:", A & B)
+
+# Difference
+print("A - B:", A - B)
+print("B - A:", B - A)
