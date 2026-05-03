802ab972008e482e77c699f582981ef9c0a96ab5
shendesaanvi-ctrl <shendesaanvi@gmail.com>
2026-05-03 19:02:11 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,19 @@
+# Create and access list
+lst = [10, 20, 30, 40]
+print("List:", lst)
+print("First element:", lst[0])
+
+# Add and remove elements
+lst.append(50)
+print("After adding:", lst)
+
+lst.remove(20)
+print("After removing:", lst)
+
+# Sort list
+lst.sort()
+print("Sorted list:", lst)
+
+# Reverse list
+lst.reverse()
+print("Reversed list:", lst)
