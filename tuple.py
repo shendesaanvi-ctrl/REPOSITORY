2ad1c6e71fabcb70279c2d9c45661bb54650204e
shendesaanvi-ctrl <shendesaanvi@gmail.com>
2026-05-03 19:05:53 +0530

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,15 @@
+# Create and access
+t = (1, 2, 3)
+print("Tuple:", t)
+print("First element:", t[0])
+
+# Nested tuple
+nested = (1, (2, 3), 4)
+print("Nested tuple:", nested)
+
+# Repetition
+print("Repetition:", t * 2)
+
+# Concatenation
+t2 = (4, 5)
+print("Concatenation:", t + t2)
